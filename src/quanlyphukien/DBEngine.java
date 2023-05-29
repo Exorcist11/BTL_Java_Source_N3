2b45da98d8f19f93d30d627b2860fdb60893c0b1
godsk1n <danghieu4280@gmail.com>
2023-05-29 18:14:01 +0700

    sua lai dbengine

--- src/quanlyphukien/DBEngine.java
+++ src/quanlyphukien/DBEngine.java
@@ -18,8 +18,8 @@ public class DBEngine {
     public void saveFile(String fileName, PhuKien obj) throws Exception {
         
         FileWriter fw = new FileWriter(fileName,true);
-        String value = String.format("%10d%10s%10s%10s%10d%10d%10d\n",obj.getId(),obj.getName(),obj.getColor(),obj.getType(),obj.getYear(),obj.getNumber(),obj.getPrice());
-        fw.write(value);
+        
+        fw.write(obj.toString());
         fw.close();
     }
     
